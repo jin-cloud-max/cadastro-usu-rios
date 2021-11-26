89355dfc99c6266fcfe434c3fbb7a0e481eb4e11
Jin <jinoliveira74@gmail.com>
2021-11-26 16:27:37 -0300

    chore:  senha encriptada

--- src/credentials/credentials.service.ts
+++ src/credentials/credentials.service.ts
@@ -2,30 +2,42 @@ import { Credentials, Prisma } from '@prisma/client';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
+import { hash } from "bcrypt"
+
 
 @Injectable()
 export class CredentialsService {
-   constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-   async create(data: Prisma.CredentialsCreateInput): Promise<Credentials> {
-      return this.prisma.credentials.create({
-         data
-      })
-   }
+  async create(data: Prisma.CredentialsCreateInput): Promise<Credentials> {
+    const { password, email, user } = data
 
-   findAll() {
-      return `This action returns all credentials`;
-   }
+    const hashedPassword = await hash(password, 10)
 
-   findOne(id: number) {
-      return `This action returns a #${id} credential`;
-   }
+    const newUser = this.prisma.credentials.create({
+      data: {
+        password: hashedPassword,
+        email,
+        user
+      }
+    })
 
-   // update(id: number, updateCredentialDto: UpdateCredentialDto) {
-   //    return `This action updates a #${id} credential`;
-   // }
+    return newUser
+  }
 
-   remove(id: number) {
-      return `This action removes a #${id} credential`;
-   }
+  findAll() {
+    return `This action returns all credentials`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} credential`;
+  }
+
+  // update(id: number, updateCredentialDto: UpdateCredentialDto) {
+  //    return `This action updates a #${id} credential`;
+  // }
+
+  remove(id: number) {
+    return `This action removes a #${id} credential`;
+  }
 }
